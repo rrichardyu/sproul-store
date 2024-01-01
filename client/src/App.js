1a90dda1d605bd0fffc8286d4264fa6da6e13f11
Richard Yu <richardyu@berkeley.edu>
2024-01-01 13:17:12 -0800

    Add proxy to development

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+const callAPIEndpoint = () => {
+  fetch("/").then((data) => {
+    console.log(data)
+  })
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>test</p>
+        <button onClick={callAPIEndpoint}>Test</button>
       </header>
     </div>
   );
