e4035454b5a11dd5910c77c1fdb13f9708948a72
Richard Yu <richardyu@berkeley.edu>
2024-01-01 12:38:55 -0800

    Add express starter code

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
