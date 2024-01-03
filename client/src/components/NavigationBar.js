9d1a71568bee8cfce8c9746fae0de24b5b6f00ec
Richard Yu <richardyu@berkeley.edu>
2024-01-03 15:26:03 -0800

    Add login button to frontend

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google"
 
 export default function NavigationBar() {
     return (
@@ -6,6 +7,19 @@ export default function NavigationBar() {
             <li><Link to="/">Listings</Link></li>
             <li><Link to="/listing/new">New Listing</Link></li>
             <li><Link to="/profile">Profile</Link></li>
+
+            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+                <GoogleLogin 
+                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                    buttonText="Sign in with Google"
+                    onSuccess={credentialResponse => {
+                        console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                        console.log("Login failed");
+                    }}
+                />
+            </GoogleOAuthProvider>
         </ul>
     )
 }
