7bb43d63a81a420b1156800bda0a7a8f6867474f
Richard Yu <richardyu@berkeley.edu>
2024-01-03 13:58:21 -0800

    Add 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Listing from "./pages/Listing"
 import Listings from "./pages/Listings"
 import Profile from "./pages/Profile"
 import NavigationBar from "./components/NavigationBar"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Listings />} />
         <Route path="/listing/:id" element={<Listing />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
