84afec56dc8454977a46420a1b10202e9e132468
Richard Yu <richardyu@berkeley.edu>
2024-01-04 12:18:28 -0800

    Left sidebar sign-in design

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,16 @@
+import Authentication from "../components/Authentication";
+
 export default function Landing() {
     return (
-        <>
-            <h1>Home</h1>
-            <p>Sign in with Google</p>
-            <p>What is sproul.store?</p>
-        </>
+        <div id="landing-container">
+            <div id="landing-content">
+                <h1 id="landing-title">sproul.store</h1>
+                <p class="landing-description">A community marketplace for students at UC Berkeley.</p>
+                <p class="landing-description">Sign in with your berkeley.edu email.</p>
+                <div id="landing-auth-btn">
+                    <Authentication />
+                </div>
+            </div>
+        </div>
     )
 }
