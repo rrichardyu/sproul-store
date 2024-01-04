a5453ce5c8efb7ff4d7c68eaa2251ddeef43b37b
Richard Yu <richardyu@berkeley.edu>
2024-01-03 17:04:09 -0800

    Add berkeley email address detection

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,7 +1,11 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google"
 
 export default function NavigationBar() {
+    const [user, setUser] = useState({})
+    const [signedIn, setSignedIn] = useState(false)
+
     const authenticate = async (payload) => {
         try {
             let headers = new Headers();
@@ -17,6 +21,8 @@ export default function NavigationBar() {
 
             const authResponseJSON = await authResponse.json()
             console.log(authResponseJSON)
+            setUser(authResponseJSON)
+            setSignedIn(true)
         } catch (err) {
             console.error(err.message)
         }
@@ -28,19 +34,28 @@ export default function NavigationBar() {
             <li><Link to="/listing/new">New Listing</Link></li>
             <li><Link to="/profile">Profile</Link></li>
 
-            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-                <GoogleLogin 
-                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                    buttonText="Sign in with Google"
-                    onSuccess={credentialResponse => {
-                        console.log(credentialResponse)
-                        authenticate(credentialResponse)
-                    }}
-                    onError={() => {
-                        console.log("Login failed")
-                    }}
-                />
-            </GoogleOAuthProvider>
+            { signedIn ? 
+                user.berkeley ? (
+                    <p>Signed in as {user.name} ({user.email})</p>
+                ) : (
+                    <p>You must sign in with a valid UC Berkeley email address (@berkeley.edu).</p>
+                )
+             : (
+                    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+                        <GoogleLogin 
+                            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                            buttonText="Sign in with Google"
+                            onSuccess={credentialResponse => {
+                                console.log(credentialResponse)
+                                authenticate(credentialResponse)
+                            }}
+                            onError={() => {
+                                console.log("Login failed")
+                            }}
+                        />
+                    </GoogleOAuthProvider>
+                )
+            }
         </ul>
     )
 }
