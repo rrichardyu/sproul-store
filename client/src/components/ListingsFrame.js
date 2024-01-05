9ab570d981a6e34e3330249f9460ede04246a451
Richard Yu <richardyu@berkeley.edu>
2024-01-04 22:01:45 -0800

    Implement working jwt token auth on listings

--- client/src/components/ListingsFrame.js
+++ client/src/components/ListingsFrame.js
@@ -1,15 +1,23 @@
 import { useEffect, useState } from 'react';
 import { Link } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 export default function Listings({ req_num_listings }) {
     const [isBusy, setBusy] = useState(true)
     const [listings, setListings] = useState([])
+    const [authState, setAuthState] = useAuth()
 
     const getListings = async (req_num_listings) => {
         try {
             const listingsResponse = await fetch("/api/listings?" + new URLSearchParams({
                 limit: req_num_listings
-            }))
+            }), 
+                {
+                    headers: {
+                        "Authorization": "Bearer " + authState.token
+                    }
+                }
+            )
             const listingsJSON = await listingsResponse.json()
             console.log(listingsJSON)
             setListings(listingsJSON)
